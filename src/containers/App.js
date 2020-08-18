3e05950de833e07d1e975a5f66831cd5f8d0ec29
jushita <jushitaa@gmail.com>
2020-08-18 16:18:24 -0500

    fixed import issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../App.css';
+import MainContainer from './MainContainer'
+import Nav from './Nav'
 
 function App() {
   return (
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContainer() {
+    return(
+        <div className="nav">
+            <h1>Main Container</h1>
+        </div>
+    )
+}
+
+export default MainContainer;
